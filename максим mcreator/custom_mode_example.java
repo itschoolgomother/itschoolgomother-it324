ba5515b1bdd92e95426cf47047233d5397ce0923
itschoolgomother <57990662+itschoolgomother@users.noreply.github.com>
2021-09-16 20:05:03 +0300

    Update custom_mode_example.java

--- максим mcreator/custom_mode_example.java
+++ максим mcreator/custom_mode_example.java
@@ -1,6 +1,4 @@
-// Made with Blockbench 3.9.3
-// Exported for Minecraft version 1.15 - 1.16 with MCP mappings
-// Paste this class into your mod and generate all required imports
+
 
 
 public class custom_model extends EntityModel<Entity> {
